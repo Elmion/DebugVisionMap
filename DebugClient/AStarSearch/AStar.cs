b7038d2123ecb26487ee8546144619f9418c4d4d
Евгений <firegeometry@gmail.com>
2018-10-14 23:08:17 +1000

    fjfghj

---
+++ DebugClient/AStarSearch/AStar.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleNet;
+using TriangleNet.Geometry;
+
+namespace DebugClient.AStarSearch
+{
+
+    class AStarPath
+    {
+        private List<Edge> edges;
+
+        public AStarPath(Mesh mesh)
+        {
+            edges = new List<Edge>(mesh.Edges);
+
+        }
+        //public void BuildCostsPath()
+        //{
+        //    NodeVisits = 0;
+        //    Start.MinCostToStart = 0;
+        //    var prioQueue = new List<Node>();
+        //    prioQueue.Add(Start);
+        //    do
+        //    {
+        //        prioQueue = prioQueue.OrderBy(x => x.MinCostToStart + x.StraightLineDistanceToEnd).ToList();
+        //        var node = prioQueue.First();
+        //        prioQueue.Remove(node);
+        //        NodeVisits++;
+        //        foreach (var cnn in node.Connections.OrderBy(x => x.Cost))
+        //        {
+        //            var childNode = cnn.ConnectedNode;
+        //            if (childNode.Visited)
+        //                continue;
+        //            if (childNode.MinCostToStart == null ||
+        //                node.MinCostToStart + cnn.Cost < childNode.MinCostToStart)
+        //            {
+        //                childNode.MinCostToStart = node.MinCostToStart + cnn.Cost;
+        //                childNode.NearestToStart = node;
+        //                if (!prioQueue.Contains(childNode))
+        //                    prioQueue.Add(childNode);
+        //            }
+        //        }
+        //        node.Visited = true;
+        //        if (node == End)
+        //            return;
+        //    } while (prioQueue.Any());
+        //}
+
+    }
+    public class Searchmap
+    {
+
+        public int NodeVisits { get; private set; }
+        public double ShortestPathLength { get; set; }
+        public double ShortestPathCost { get; private set; }
+
+
+        public List<Node> Nodes { get; set; } = new List<Node>();
+
+        public Node StartNode { get; set; }
+
+        public Node EndNode { get; set; }
+
+        public List<Node> ShortestPath { get; set; } = new List<Node>();
+
+
+        public void AstarSearch()
+        {
+           // NodeVisits = 0;
+            StartNode.MinCostToStart = 0;
+            var prioQueue = new List<Node>();
+            prioQueue.Add(StartNode);
+            do
+            {
+                prioQueue = prioQueue.OrderBy(x => x.MinCostToStart + x.StraightLineDistanceToEnd).ToList();
+                var node = prioQueue.First();
+                prioQueue.Remove(node);
+                //NodeVisits++;
+                foreach (var cnn in node.Connections.OrderBy(x => x.Cost))
+                {
+                    var childNode = cnn.ConnectedNode;
+                    if (childNode.Visited)
+                        continue;
+                    if (childNode.MinCostToStart == null ||
+                        node.MinCostToStart + cnn.Cost < childNode.MinCostToStart)
+                    {
+                        childNode.MinCostToStart = node.MinCostToStart + cnn.Cost;
+                        childNode.NearestToStart = node;
+                        if (!prioQueue.Contains(childNode))
+                            prioQueue.Add(childNode);
+                    }
+                }
+                node.Visited = true;
+                if (node == EndNode)
+                    return;
+            } while (prioQueue.Any());
+        }
+
+        public List<Node> GetShortestPathAstart()
+        {
+            foreach (var node in Nodes)
+                node.StraightLineDistanceToEnd = node.StraightLineDistanceTo(StartNode);
+            AstarSearch();
+            var shortestPath = new List<Node>();
+            shortestPath.Add(StartNode);
+            BuildShortestPath(shortestPath, StartNode);
+            shortestPath.Reverse();
+            return shortestPath;
+        }
+
+        private void BuildShortestPath(List<Node> list, Node node)
+        {
+            if (node.NearestToStart == null)
+                return;
+            list.Add(node.NearestToStart);
+            ShortestPathLength += node.Connections.Single(x => x.ConnectedNode == node.NearestToStart).Length;
+            ShortestPathCost += node.Connections.Single(x => x.ConnectedNode == node.NearestToStart).Cost;
+            BuildShortestPath(list, node.NearestToStart);
+        }
+
+
+    }
+
+
+    public class Node : Vertex
+    {
+       // public Guid Id { get; set; }
+       // public string Name { get; set; }
+        public Vertex Point { get; set; }
+        public List<SearchEdge> Connections { get; set; } = new List<SearchEdge>();
+
+        public double? MinCostToStart { get; set; }
+        public Node NearestToStart { get; set; }
+        public bool Visited { get; set; }
+        public double StraightLineDistanceToEnd { get; set; }
+
+        public static List<Node> CreateListNodes(ref Mesh map)
+        {
+            int index = 0;
+            List<Node> nodes = new List<Node>();
+            Vertex[] verx = map.Vertices.ToArray();
+            for (int i = 0; i < verx.Length; i++)
+            {
+                Node node = new Node { Point = verx[i], Visited = false, Connections = new List<SearchEdge>() };
+                List<Edge> list = map.Edges.Where(x => x.P0 == index || x.P1 == index).ToList();
+
+                foreach (Edge item in list)
+                {
+                    node.Connections.Add(new SearchEdge {
+                                     ConnectedNode = node,
+                                     Cost = 1,
+                                     Length = Math.Pow(verx[item.P0].X - verx[item.P1].X, 2) + Math.Pow(verx[item.P0].Y - verx[item.P1].Y, 2)
+                                    });
+                }
+                nodes.Add(node);
+            }
+            return nodes;
+        }
+        internal void ConnectClosestNodes(ref Mesh map)
+        {
+        }
+        internal void ConnectClosestNodes(List<Node> nodes, int branching, Random rnd, bool randomWeight)
+        {
+            var connections = new List<SearchEdge>();
+            foreach (var node in nodes)
+            {
+                //if (node.Id == this.Id)
+                //    continue;
+
+                // var dist = Math.Sqrt(Math.Pow(Point.X - node.Point.X, 2) + Math.Pow(Point.Y - node.Point.Y, 2));
+                var dist = Math.Pow(Point.X - node.Point.X, 2) + Math.Pow(Point.Y - node.Point.Y, 2);
+                connections.Add(new SearchEdge
+                {
+                    ConnectedNode = node,
+                    Length = dist,
+                    Cost = randomWeight ? rnd.NextDouble() : dist,
+                });
+            }
+            connections = connections.OrderBy(x => x.Length).ToList();
+            var count = 0;
+            foreach (var cnn in connections)
+            {
+                //Connect three closes nodes that are not connected.
+                if (!Connections.Any(c => c.ConnectedNode == cnn.ConnectedNode))
+                    Connections.Add(cnn);
+                count++;
+
+                //Make it a two way connection if not already connected
+                if (!cnn.ConnectedNode.Connections.Any(cc => cc.ConnectedNode == this))
+                {
+                    var backConnection = new SearchEdge { ConnectedNode = this, Length = cnn.Length };
+                    cnn.ConnectedNode.Connections.Add(backConnection);
+                }
+                if (count == branching)
+                    return;
+            }
+        }
+
+        public double StraightLineDistanceTo(Node end)
+        {
+            // return Math.Sqrt(Math.Pow(Point.X - end.Point.X, 2) + Math.Pow(Point.Y - end.Point.Y, 2));
+            return Math.Pow(Point.X - end.Point.X, 2) + Math.Pow(Point.Y - end.Point.Y, 2);
+        }
+
+        internal bool ToCloseToAny(List<Node> nodes)
+        {
+            foreach (var node in nodes)
+            {
+                // var d = Math.Sqrt(Math.Pow(Point.X - node.Point.X, 2) + Math.Pow(Point.Y - node.Point.Y, 2));
+                var d = Math.Pow(Point.X - node.Point.X, 2) + Math.Pow(Point.Y - node.Point.Y, 2);
+                if (d < 0.01)
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    public class SearchEdge
+    {
+        public double Length { get; set; }
+        public double Cost { get; set; }
+        public Node ConnectedNode { get; set; }
+    }
+}
--- DebugClient/Engine.cs
+++ DebugClient/Engine.cs
@@ -8,7 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using UnityEngine;
-
+using TriangleNet.Geometry;
+using TriangleNet;
+using TriangleNet.Rendering;
+using TriangleNet.Meshing;
+using DebugClient.AStarSearch;
 namespace DebugClient
 {
     public partial class Engine : Form
@@ -18,6 +22,13 @@ namespace DebugClient
         private List<object> SceneObjects;
         private Hero Hero;
 
+
+        RenderManager renderManager;
+        private TriangleNet.Mesh mesh;
+        private IPolygon input;
+
+        private Vertex start, end;
+
         public Engine()
         {
             InitializeComponent();
@@ -25,6 +36,29 @@ namespace DebugClient
             //Heros = new List<Hero>();
             Instance = this;
             this.DoubleBuffered = true;
+
+            renderManager = new RenderManager();
+            IRenderControl control = new TriangleNet.Rendering.GDI.RenderControl();
+
+            InitializeRenderControl((Control)control);
+            renderManager.Initialize(control);
+            ((TriangleNet.Rendering.GDI.RenderControl)control).OnRenderLeftClick += UpdateStartPoint;
+            ((TriangleNet.Rendering.GDI.RenderControl)control).OnRenderRightClick += UpdateEndPoint;
+
+            this.start = new Vertex(-25, -25);
+            this.end = new Vertex(0, 35);
+            UpdateMesh(this.start, this.end);
+
+            var r = Node.CreateListNodes(ref mesh);
+
+            Searchmap a = new Searchmap { Nodes = r, StartNode = r[0], EndNode = r[1] } ;
+            a.AstarSearch();
+            var e = a.GetShortestPathAstart();
+
+
+            //input = new BoxWithHole().Generate(new Vertex(-25,-25),new Vertex(0,35));
+            //renderManager.Set(input);
+            //Triangulate();
 
             time = new Timer();
             time.Interval = 1;
@@ -35,6 +69,7 @@ namespace DebugClient
           
 
             time.Start();
+
         }
         public T CreateSceneObject<T>(params object[] parm)
         {
@@ -69,5 +104,83 @@ namespace DebugClient
                 Hero.MoveTo(new Vector2(e.X, e.Y));
             }
         }
+        private void UpdateStartPoint(Vertex start)
+        {
+            this.start = start;
+            UpdateMesh(this.start, this.end);
+        }
+        private void UpdateEndPoint(Vertex end)
+        {
+            this.end = end;
+            UpdateMesh(this.start, this.end);
+        }
+        private void UpdateMesh(Vertex start,Vertex end)
+        {
+            input = new BoxWithHole().Generate(start, end);
+            renderManager.Set(input);
+            Triangulate();
+        }
+        private void InitializeRenderControl(Control control)
+        {
+            this.SuspendLayout();
+            this.Controls.Add(control);
+
+            var size = this.ClientRectangle;
+
+            // Initialize control
+            control.BackColor = System.Drawing.Color.Black;
+            control.Dock = DockStyle.Fill;
+            control.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+            control.Location = new System.Drawing.Point(0, 0);
+            control.Name = "renderControl1";
+            control.Size = new Size(size.Width, size.Height);
+            control.TabIndex = 0;
+            control.Text = "renderControl1";
+
+            this.ResumeLayout();
+        }
+        private void Triangulate()
+        {
+            if (input == null) return;
+
+            var options = new ConstraintOptions();
+            var quality = new QualityOptions();
+
+            mesh = (TriangleNet.Mesh)input.Triangulate(options, quality);
+            
+            renderManager.Set(mesh, true);
+        }
     }
+    public class BoxWithHole : BaseGenerator
+    {
+        public BoxWithHole()
+        {
+            name = "Box with Hole";
+            description = "";
+            parameter = 3;
+
+            descriptions[0] = "Points on box sides:";
+            descriptions[1] = "Points on hole:";
+            descriptions[2] = "Radius:";
+
+            ranges[0] = new int[] { 5, 50 };
+            ranges[1] = new int[] { 10, 200 };
+            ranges[2] = new int[] { 5, 20 };
+        }
+
+        public override IPolygon Generate(Vertex start, Vertex end)
+        {
+            var input = new Polygon();
+
+            input.Add(start);
+            input.Add(end);
+
+            input.Add(new Contour(CreateRectangle(new TriangleNet.Geometry.Rectangle(-20, -20, 40, 40), 1)), true);
+            input.Add(new Contour(CreateRectangle(new TriangleNet.Geometry.Rectangle(-15, 30, 10, 10), 1)), true);
+            input.Add(new Contour(CreateRectangle(new TriangleNet.Geometry.Rectangle(-50, -50, 100, 100), 1)), false);
+            return input;
+        }
+    }
+   
+
 }
